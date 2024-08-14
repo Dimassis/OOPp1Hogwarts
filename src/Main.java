6f20f67e35ff5125825f6b63a75499e05f52c3aa
Dimassis <166415819+Dimassis@users.noreply.github.com>
2024-08-14 12:26:36 +0500

    Corrected

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+ public class Main {
+            public static void main(String[] args) {
+                Pupils pupils = new Pupils();
+                pupils.descriptionPupil("Harry Potter");
+                pupils.descriptionPupil("Marcus Belby");
+                pupils.comparisonOutsideFaculty("Harry Potter", "Marcus Belby");
+                pupils.comparisonInsideFaculty("Padma Patil", "Zhou Chang");
+            }
+        }
